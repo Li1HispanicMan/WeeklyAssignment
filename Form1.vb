297b882dffe19d75d7d6e3d325fc51883c3b88ae
li1hi <li1hi@DESKTOP-QGMSE2E>
2023-02-07 17:29:33 -0600

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmBurgers
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+    End Sub
+
+    Private Sub frmBurger_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblInstructions.Click
+
+    End Sub
+End Class
